66ef6718c25b6c196570701592c9eb3208a8a07a
Nguyễn Lê Tiến Đạt <23130050@st.hcmuaf.edu.vn>
2026-03-14 11:03:57 +0700

    them trang admin quan ly danh gia, model Product review

---
+++ src/main/java/com/example/do_an_ttltweb/model/ProductReview.java
@@ -0,0 +1,92 @@
+package com.example.do_an_ttltweb.model;
+
+import java.sql.Timestamp;
+
+public class ProductReview {
+    private int id;
+    private int productId;
+    private int userId;
+    private int rating;
+    private String comment;
+    private Timestamp created_at;
+    private String username;
+    private String productname;
+
+
+    public ProductReview() {
+    }
+
+    public ProductReview(int id, int productId, int userId, int rating, String comment, Timestamp created_at) {
+        this.id = id;
+        this.productId = productId;
+        this.userId = userId;
+        this.rating = rating;
+        this.comment = comment;
+        this.created_at = created_at;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+}
