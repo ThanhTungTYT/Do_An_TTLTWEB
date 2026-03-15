81f8124ecf16c9c1ab0601c9ee7c766e33ec17f3
NguyenHuyBao11-05 <23130022@st.hcmuaf.edu.vn>
2026-03-15 21:40:57 +0700

    fix(bug): Sua loi error 404 khong hien thi trang

--- out/artifacts/Do_An_TTLTWEB_war_exploded/assets/js/aboutUs.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", () => {
-    const elements = document.querySelectorAll(".fade-in, .slide-up");
-    elements.forEach((el) => {
-        const rect = el.getBoundingClientRect();
-        if (rect.top < window.innerHeight - 100) {
-            el.classList.add("visible");
-        }
-    });
-});
